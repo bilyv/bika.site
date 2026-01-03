30a41b48d1884835c1185cd31abb7d2826b37df1
Ntwari Brian <ntwaribrian262@gmail.com>
2026-01-03 03:35:51 +0200

    fixing

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  experimental: {
+    reactCompiler: true,
+  },
+  // Set the root to the current directory to stop scanning upwards for lockfiles
+  // @ts-ignore
+  turbopack: {
+    root: ".",
+  },
 };
 
 export default nextConfig;
